fd06b423faf5ca3e74d86d81d81cef03edb387fa
Dennis Miller <enlightendata@users.noreply.github.com>
2017-12-10 10:21:32 -0800

    Update price.js

--- price.js
+++ price.js
@@ -1,18 +1,15 @@
 $.ajax({
         async: true,
         type: "GET",
-//         url: "https://cors.io/?https://www.bitstamp.net/api/ticker/",
-//         url: "https://cors-anywhere.herokuapp.com/https://www.bitstamp.net/api/ticker/",
              url: "https://api.coindesk.com/v1/bpi/currentprice.json",
         success: function(result) {
           btcData = JSON.parse(result);
           btcPrice = parseFloat(btcData.bpi.USD.rate.replace(",","")).toFixed(2);
           document.getElementById("btc").innerHTML = 'The latest BitCoin price is: $'.concat(btcPrice);
                 
-          responsiveVoice.speak("The latest Bitcoin price is ", "Russian Female");
-          responsiveVoice.speak('$'.concat(btcPrice), "US English Female");
+          responsiveVoice.speak("The latest Bitcoin price is ".concat(btcPrice), "UK English Female");
                
-          responsiveVoice.speak("hashtag pyramid scheme", "US English Male", {volume: 0.5});
+          responsiveVoice.speak("hashtag pyramid scheme", "UK English Male", {volume: 0.5});
                 
           }
 });
