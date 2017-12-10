96083055889189e1a3a392d0e236db17c5cc1ee0
Dennis Miller <enlightendata@users.noreply.github.com>
2017-12-10 09:42:59 -0800

    Update price.js

--- price.js
+++ price.js
@@ -1,7 +1,8 @@
 $.ajax({
         async: true,
         type: "GET",
-        url: "https://cors.io/?https://www.bitstamp.net/api/ticker/",
+//         url: "https://cors.io/?https://www.bitstamp.net/api/ticker/",
+        url: "https://crossorigin.me/?https://www.bitstamp.net/api/ticker/",
         success: function(result) {
           btcData = JSON.parse(result);
           data = btcData.last;
