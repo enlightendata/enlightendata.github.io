afb343113dd0f599324cbda8b9b9f65ab050a71b
Dennis Miller <enlightendata@users.noreply.github.com>
2017-12-10 10:49:36 -0800

    Update price.js

--- price.js
+++ price.js
@@ -1,20 +1,18 @@
-$.ajax({
-        async: true,
-        type: "GET",
-             url: "https://api.coindesk.com/v1/bpi/currentprice.json",
-        success: function(result) {
-          var btcData = JSON.parse(result);
-          var btcPrice = parseFloat(btcData.bpi.USD.rate.replace(",","")).toFixed(0);
-          var content = 'The latest Bitcoin price is: $'.concat(btcPrice);
-          document.getElementById("btc").innerHTML = content;
-                
-          responsiveVoice.speak(content, "UK English Female");
-          responsiveVoice.speak("hashtag pyramid scheme", "UK English Male", {volume: 0.5});
-                
-          }
-});
 
-function listenAgain() {
-          responsiveVoice.speak(content, "UK English Female");
-          responsiveVoice.speak("hashtag pyramid scheme", "UK English Male", {volume: 0.5});       
+function getBTCPrice() {
+        $.ajax({
+                async: true,
+                type: "GET",
+                     url: "https://api.coindesk.com/v1/bpi/currentprice.json",
+                success: function(result) {
+                  var btcData = JSON.parse(result);
+                  var btcPrice = parseFloat(btcData.bpi.USD.rate.replace(",","")).toFixed(0);
+                  var content = 'The latest Bitcoin price is: $'.concat(btcPrice);
+                  document.getElementById("btc").innerHTML = content;
+
+                  responsiveVoice.speak(content, "UK English Female");
+                  responsiveVoice.speak("hashtag pyramid scheme", "UK English Male", {volume: 0.5});
+
+                  }
+        });
 }
