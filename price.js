bafda62a7d83815cb9065f78af19b540d8fe7a86
Dennis Miller <enlightendata@users.noreply.github.com>
2017-12-10 06:53:50 -0800

    Update price.js

--- price.js
+++ price.js
@@ -7,7 +7,7 @@ $.ajax({
           data = btcData.last;
           document.getElementById("btc").innerHTML = 'Latest BitCoin price is: $'.concat(data);
                 
-          responsiveVoice.speak("Bitcoin is now at ", "Romanian Male");
+          responsiveVoice.speak("Bitcoin is now at ", "Bosnian Male");
           responsiveVoice.speak('$'.concat(data), "US English Male");
           }
 });
